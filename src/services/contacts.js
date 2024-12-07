25616b1b20d4086c3d507b0e6060ac892d1c6e28
Dariashevc <151487077+Dariashevc@users.noreply.github.com>
2024-12-07 00:19:45 -0700

    commit

---
+++ src/services/contacts.js
@@ -0,0 +1,38 @@
+import { ContactsCollection } from '../db/models/model.js';
+
+export const getAllContacts = async () => {
+  const contacts = await ContactsCollection.find();
+  return contacts;
+};
+
+export const getContactById = async (contactId) => {
+  const contact = await ContactsCollection.findById(contactId);
+  return contact;
+};
+
+export const createContact = async (payload) => {
+  const contact = await ContactsCollection.create(payload);
+  return contact;
+};
+
+export const updateContact = async (contactId, payload, options = {}) => {
+  const updatedContact = await ContactsCollection.findOneAndUpdate(
+    { _id: contactId },
+    payload,
+    {
+      new: true,
+    },
+  );
+
+  if (!updatedContact) return null;
+
+  return updatedContact;
+};
+
+export const deleteContact = async (contactId) => {
+  const contact = await ContactsCollection.findOneAndDelete({
+    _id: contactId,
+  });
+
+  return contact;
+};
