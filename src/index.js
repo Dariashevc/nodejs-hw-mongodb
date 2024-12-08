92a3542a4b07e8960bb9d945c2f599011ba6dc6e
Dariashevc <151487077+Dariashevc@users.noreply.github.com>
2024-12-08 00:09:54 -0700

    commit

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import { setupServer } from './server.js';
-import { initMongoConnections } from './db/initMongoConnection.js';
-const boostrap = async () => {
-  await initMongoConnections();
-  setupServer();
-};
-
-boostrap();
