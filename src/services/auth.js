12fb96d31c666a46351ffbba2e1c947761db6860
Dariashevc <151487077+Dariashevc@users.noreply.github.com>
2024-12-09 15:38:45 -0700

    commit

--- src/services/auth.js
+++ src/services/auth.js
@@ -2,8 +2,8 @@ import { ObjectId } from 'mongodb';
 import createHttpError from "http-errors";
 import bcrypt from "bcrypt";
 import { randomBytes } from "crypto";
-import UserCollection from "../db/models/User.js";
-import SessionCollection from "../db/models/Session.js";
+import UserCollection from "../db/models/user.js";
+import SessionCollection from "../db/models/session.js";
 import { accessTokenLifetime, refreshTokenLifetime } from "../constants/users.js";
 
 const createSession = () => {
